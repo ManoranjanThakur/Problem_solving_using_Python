6ab6b21e3b563197495572e3ce09fd17d6b9bfd4
Manoranjan Kumar Thakur <67204309+ManoranjanThakur@users.noreply.github.com>
2021-05-13 03:16:28 +0530

    Add files via upload

---
+++ Permutations.py
@@ -0,0 +1,14 @@
+def permutations(word):
+    if len(word)==1:
+        return([word])
+    
+    perms=permutations(word[1:])
+    char=word[0]
+    result=[]
+
+    for perm in perms:
+        for i in range(len(perms)+1):
+            result.append(perm[:i]+char+perm[i:])
+    return result
+
+print(permutations('acd'))
