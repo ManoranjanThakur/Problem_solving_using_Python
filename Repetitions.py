fcb1c56910a57a1cb83b484d35b15e8ce929f327
Manoranjan Kumar Thakur <67204309+ManoranjanThakur@users.noreply.github.com>
2021-05-14 01:29:36 +0530

    Add files via upload

---
+++ Repetitions.py
@@ -0,0 +1,14 @@
+def Repetitions(arr):
+    count=1
+    maximum=1
+    for i in range(len(arr)):
+        if (i<len(arr)-1):
+            if(arr[i]==arr[i+1]):
+                count+=1
+                if count>maximum:
+                    maximum=count
+            else:
+                count=1
+    return maximum
+
+print(Repetitions(input()))
