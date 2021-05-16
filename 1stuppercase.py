601d7c647ecfe3de12f017ca5cfac48ad91b4c8e
Manoranjan Kumar Thakur <67204309+ManoranjanThakur@users.noreply.github.com>
2021-05-17 05:24:20 +0530

    Add files via upload

---
+++ 1stuppercase.py
@@ -0,0 +1,11 @@
+A='Work'
+def recurf(A):
+    if len(A)==0:
+        return ""
+    if A[0].isupper() :
+        return A[0]
+    return recurf(A[1:])
+
+print(recurf(A))
+B='work'
+print(recurf(B))
